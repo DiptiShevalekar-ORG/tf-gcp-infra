2ddb474e3cca6db4fddce61aa741c5a9648d83f0
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-15 15:21:02 -0500

    Pushing terraform changes

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "credentialsFile" {
+  default = "credentials.json"
+}
+
+variable "projectId" {
+  default = "cloudassignment03-413923"
+}
+
+variable "region" {
+  default     = "us-west2"
+}
+
+
+variable "num_vpc" {
+  description = "Number of VPCs to create"
+  default="1"
+}
+
+variable "num_webapp_subnets_per_vpc" {
+  description = "VPC number to create"
+  default="1"
+}
+
+variable "num_db_subnets_per_vpc" {
+  description = " subnets number to create per VPC"
+  default="1"
+}
