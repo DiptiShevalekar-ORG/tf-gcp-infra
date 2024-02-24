72251cb3589c4028767acfafcdb9ae7efc40a114
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-02-23 21:49:06 -0500

    terraform

--- main.tf
+++ main.tf
@@ -4,12 +4,15 @@ provider "google" {
   region      = var.region
 }
 
+# locals {
+#   vpc_names = [for i in range(var.num_vpc) : "csye-vpc-${i}"]
+# }
 
 resource "google_compute_network" "vpc" {
- 
-  name                            = "webapp-vpc"
-  auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
+
+  name                    = "webapp-vpc"
+  auto_create_subnetworks = false
+  routing_mode            = var.mode
   delete_default_routes_on_create = true
 }
 
@@ -17,35 +20,40 @@ resource "google_compute_subnetwork" "webapp_subnet" {
   name          = "webapp-subnet"
   region        = var.region
   network       = google_compute_network.vpc.self_link
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = var.webapp_cIDR_Range  
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-
   name          = "db-subnet"
   region        = var.region
   network       = google_compute_network.vpc.self_link
-  ip_cidr_range = "10.100.0.0/24"
+  ip_cidr_range = var.DB_CIDR_Range
 }
 
 resource "google_compute_route" "webapp_route" {
-
-  name             = "webapp-route"
-  network          = google_compute_network.vpc.self_link
-  dest_range       = "0.0.0.0/0"
-  next_hop_gateway = "default-internet-gateway"
-  priority         = 1000
-  tags             = ["webapp-subnet"]
+  name                  = "webapp-route"
+  network               = google_compute_network.vpc.self_link
+  dest_range            = "0.0.0.0/0"
+  next_hop_gateway      = "default-internet-gateway"
+  priority              = 1000
+  tags                  = ["webapp"]
 }
 
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
+resource "google_compute_address" "gcpcomputeaddress" {
+  project = var.projectId
+  name = var.gcpcomputeaddressName
+  address_type =  "EXTERNAL"
+  ip_version = var.IPVersion
+}
+
+resource "google_compute_instance" "instance" {
+  name         = var.vmname
   machine_type = "n2-standard-2"
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "projects/cloudassignment03-413923/global/images/packer-1708561578"
+      image = var.ImagePath
       size  = 100
       type  = "pd-balanced"
     }
@@ -54,25 +62,32 @@ resource "google_compute_instance" "default" {
   network_interface {
     network    = google_compute_network.vpc.self_link
     subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+      nat_ip = google_compute_address.gcpcomputeaddress.address
+    }
   }
 
   service_account {
-    email  = "csye-gcp-assignment04-sa@cloudassignment03-413923.iam.gserviceaccount.com"
+    email  = var.email
     scopes = ["cloud-platform"]
   }
+
+  tags = ["webapp"]
+
 }
 
-resource "google_compute_firewall" "rules" {
-  project     = var.projectId
-  name        = "my-firewall-rule"
-  network     = google_compute_network.vpc.self_link
-  description = "Creates firewall rule targeting tagged instances"
+resource "google_compute_firewall" "vmfirewallrule" {
 
+  name    = var.firewallRuleName
+  network = google_compute_network.vpc.self_link
   allow {
     protocol = "tcp"
-    ports    = ["3002"]
+    ports    = [var.Port]
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["webapp-subnet"]
+  priority  = 1000
+  direction = "INGRESS"
+
+  source_ranges = ["0.0.0.0/0", "35.235.240.0/20"]
+  target_tags   = ["webapp"]
 }
