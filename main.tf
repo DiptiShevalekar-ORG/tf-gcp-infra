ae6aa23742f4462f7d60632149aa84f27c0bfb41
Dipti Shevalekar <shevalekar.d@northeastern.edu>
2024-03-27 17:28:01 -0400

    pushing terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file(var.credentialsFile)
   project     = var.projectId
   region      = var.region
 }
@@ -9,7 +8,7 @@ provider "google" {
 # }
 
 resource "google_compute_network" "vpc" {
-  name                            = "webappp-vpc-1"
+  name                            = "webapp-vpc-2"
   auto_create_subnetworks         = false
   routing_mode                    = var.mode
   delete_default_routes_on_create = true
@@ -57,7 +56,7 @@ resource "google_compute_firewall" "vmfirewallrule" {
   }
 
   priority  = 1000
-  direction = var.InOutGress
+  //direction = var.InOutGress
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["webapp"]
@@ -65,7 +64,7 @@ resource "google_compute_firewall" "vmfirewallrule" {
 
 
 resource "google_compute_global_address" "private_ip_block" {
-  name          = "private-ip-block1"
+  name          = "private-ip-block2"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   ip_version    = var.IPVersion
@@ -114,10 +113,9 @@ resource "google_sql_database" "webapp" {
 
 resource "random_password" "password" {
   length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+  special          = false
+  //override_special = "!#$%&*()-_=+[]{}<>:?"
 }
-
 
 resource "google_sql_user" "webapp" {
   name     = "webapp"
@@ -147,10 +145,57 @@ resource "google_project_iam_binding" "vm_loggingadmin" {
   members = ["serviceAccount:${google_service_account.service_account_iam.email}"]
 }
 
+resource "google_project_iam_binding" "vm-opsagent-publisher" {
+  project = var.projectId
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${google_service_account.service_account_iam.email}"]
+}
+
 resource "google_project_iam_binding" "vm_metricswriter" {
   project = var.projectId
   role    = "roles/monitoring.metricWriter"
   members = ["serviceAccount:${google_service_account.service_account_iam.email}"]
+}
+
+resource "google_service_account" "service_account_cloudfunction" {
+  account_id   = "logger-sa-assignment07"
+  display_name = "logger-sa-assignment07"
+  depends_on = [ google_pubsub_topic.verify_email ]
+}
+
+resource "google_project_iam_binding" "pubsub-publisher" {
+  project = var.projectId
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${google_service_account.service_account_cloudfunction.email}"]
+}
+
+resource "google_project_iam_binding" "pubsub-clf-invoker" {
+  project = var.projectId
+  role    = "roles/cloudfunctions.invoker"
+  members = ["serviceAccount:${google_service_account.service_account_cloudfunction.email}"]
+}
+resource "google_project_iam_binding" "pubsub-clf-client" {
+  project = var.projectId
+  role    = "roles/cloudsql.client"
+  members = ["serviceAccount:${google_service_account.service_account_cloudfunction.email}"]
+}
+
+resource "google_project_iam_binding" "pubsub-clf-subs" {
+  project = var.projectId
+  role    = "roles/pubsub.subscriber"
+  members = ["serviceAccount:${google_service_account.service_account_cloudfunction.email}"]
+}
+
+resource "google_project_iam_binding" "cloud-function-invoker" {
+  project = var.projectId
+  role    = "roles/run.invoker"
+  members = ["serviceAccount:${google_service_account.service_account_cloudfunction.email}"]
+}
+
+resource "google_project_iam_binding" "cloud-vpc-access-user" {
+  project = var.projectId
+  role    = "roles/vpcaccess.user"
+  members = ["serviceAccount:${google_service_account.service_account_cloudfunction.email}"]
 }
 
 #setting iam role to service account
@@ -166,7 +211,7 @@ resource "google_compute_instance" "instance" {
   name         = var.vmname
   machine_type = var.machineType
   zone         = var.zone
-  depends_on = [google_project_iam_binding.vm_metricswriter]
+  depends_on   = [google_project_iam_binding.vm_metricswriter]
 
   boot_disk {
     initialize_params {
@@ -187,13 +232,15 @@ resource "google_compute_instance" "instance" {
 
   service_account {
     email  = google_service_account.service_account_iam.email
-    scopes = ["cloud-platform"]
+    scopes = ["cloud-platform", "pubsub"]
   }
 
   tags = ["webapp"]
 
   metadata_startup_script = <<-SCRIPT
+  
 #!/bin/bash
+
 
 sudo bash -c 'cat <<EOF > /opt/webappUnzipped/Dipti_Shevalekar_002245703_01/.env
 DB_USERNAME=${google_sql_user.webapp.name}
@@ -202,10 +249,81 @@ PASSWORD=${random_password.password.result}
 HOST=${google_sql_database_instance.csye6225.private_ip_address}
 PORT=3000
 EOF'
-
+sudo chown -R csye6225:csye6225 opt/webappUnzipped/Dipti_Shevalekar_002245703_01
+sudo chmod -R 755 opt/webappUnzipped/Dipti_Shevalekar_002245703_01
 sudo systemctl restart systemdSetup.service
+
 
 SCRIPT
 
 }
 
+resource "google_vpc_access_connector" "my_connector" {
+  name            = "my-vpc-connector"
+  network         = google_compute_network.vpc.self_link
+  ip_cidr_range   = "10.8.0.0/28"
+}
+
+
+resource "google_pubsub_topic" "verify_email" {
+  name                       = "verify_email"
+  message_retention_duration = "604800s"
+  # message_storage_policy {
+  #   allowed_persistence_regions = [var.region]
+  # }
+}
+
+# resource "google_pubsub_subscription" "verify_email_sub" {
+#   name  = "verify-email-sub"
+#   topic = google_pubsub_topic.verify_email.name
+#   # push_config {
+#   #   push_endpoint = "https://your-cloud-function-url"
+#   # }
+# }
+
+resource "google_cloudfunctions2_function" "verify_email" {
+  name        = var.cloud_function_name
+  location    = var.region
+  description = "Send verification emails"
+
+  build_config {
+    runtime     = var.cloud_function_runtime
+    entry_point = var.entry_point
+    source {
+      storage_source {
+        bucket = "cloud-csye6225-bucket"
+        object = "FORK_serverless.zip"
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count    = 1
+    min_instance_count    = 1
+    available_cpu         = "1"
+    available_memory      = var.cloud_function_available_memory_mb
+    timeout_seconds       = var.cloud_function_timeout
+    service_account_email = google_service_account.service_account_cloudfunction.email
+    vpc_connector         = google_vpc_access_connector.my_connector.self_link
+
+    environment_variables = {
+      MAILGUN_API_KEY  = var.MAILGUN_API_KEY
+      WEBAPP_URL       = var.WEBAPP_URL
+      DB_USERNAME=google_sql_user.webapp.name
+      DATABASE=google_sql_database.webapp.name
+      PASSWORD=random_password.password.result
+      HOST=google_sql_database_instance.csye6225.private_ip_address
+    }
+
+  }
+
+  event_trigger {
+    event_type            = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic          = google_pubsub_topic.verify_email.id
+    retry_policy          = "RETRY_POLICY_RETRY"
+    trigger_region        = "us-east1"
+    service_account_email = google_service_account.service_account_cloudfunction.email
+  }
+
+  depends_on = [google_pubsub_topic.verify_email]
+}
